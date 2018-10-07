192fc833b090e0400d374be87447ad1355523356
SergeiKaptelin <serhii.kaptelin@gmail.com>
2018-10-07 18:27:59 +0300

    add play logic

--- src/index.js
+++ src/index.js
@@ -8,34 +8,9 @@ import "normalize.css/normalize.css";
 import "./styles/index.scss";
 import {generateGoal} from "./puzzle/Puzzle";
 
+const MOVE_DISTANCE = "83px";
+
 document.addEventListener("DOMContentLoaded", () => {
-  let npuzzle = {
-    heuristic: "manh",
-    field: "3x3",
-    size: 3,
-    puzzle: [[]],
-    emptyRow: 2,
-    emptyCol: 2,
-  };
-
-  $("#3x3").click(() => handleDrawField("3x3"));
-  $("#4x4").click(() => handleDrawField("4x4"));
-  $("#manhattanDistance").click(() => npuzzle.heuristic = "manh");
-  $("#linearConflicts").click(() => npuzzle.heuristic = "lin");
-  $("#misplacedTiles").click(() => npuzzle.heuristic = "misp");
-  $(".random-button").click(() => handleRandomizeField());
-  $(".solve-button").click(() => handleSolve());
-
-
-  $("#solve").click(start);
-  $("#show-solutions").click(showSolution);
-
-  var emptytilePosRow = 1;
-  var emptytilePosCol = 2;
-  var cellDisplacement = "69px";
-
-  $(".start .cell").click(moveTile);
-
   const handleDrawField = (fieldName) => {
     const largeField = $(".largeField");
     const smallField = $(".smallField");
@@ -63,6 +38,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const astar = new AStar(init, goal, 0);
     $("#loading").removeClass("hide");
     npuzzle.field === "3x3" ? resolvePuzzle(astar) : setTimeout(() => resolvePuzzle(astar), 100);
+    $(".play-wrap").fadeIn();
+    $(".solve-button").addClass("disabled-button");
   };
 
   const handleRandomizeField = () => {
@@ -70,17 +47,42 @@ document.addEventListener("DOMContentLoaded", () => {
     npuzzle.puzzle = puzzleGenerator.generate();
     $(".result").fadeOut();
     insertRandomizedField(npuzzle.puzzle);
+    $(".play-wrap").fadeOut();
+    $(".solve-button").removeClass("disabled-button");
+    $(".play-button").removeClass("disabled-button");
+    $("#heuristic-select").removeClass("disabled-button");
+  };
+
+  const handlePlay = () => {
+    let domField = undefined;
+    switch (npuzzle.field) {
+      case "3x3":
+        domField = $(".smallField");
+        break;
+      case "4x4":
+        domField = $(".largeField");
+        break;
+    }
+    movePeace(domField);
+    $(".play-button").addClass("disabled-button");
+    $("#heuristic-select").addClass("disabled-button");
+  };
+
+  const handleChangeHeuristic = (heuriscit) => {
+    npuzzle.heuristic = heuriscit;
+    $(".solve-button").removeClass("disabled-button");
   };
 
   const resolvePuzzle = (astar) => {
     const startTime = new Date();
     const result = astar.execute(npuzzle.heuristic);
     const endTime = new Date();
+    npuzzle.path = result.path.split("");
     $("#loading").addClass("hide");
     $(".complexity-in-time").text(`${result.complexityInTime}`);
     $(".complexity-in-size").text(`${result.complexityInSize}`);
     $(".number-of-moves").text(`${result.path.length}`);
-    $(".sequence-of-states").text(`${result.path.split("").join("-")}`);
+    $(".sequence-of-states").text(`${npuzzle.path.join("-")}`);
     $(".completed-in").text(`${endTime - startTime} ms`);
     $(".result").fadeIn();
   };
@@ -99,9 +101,14 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!domField) {
       return;
     }
+    
+    const rows = domField.find(".row");
+    $.each(rows, (i, row) => $.each(row.children, (j, cell) => $(cell).attr("data-pos", `${i},${j}`)));
+
     for (let i = 0; i < puzzle.length; i++) {
       for (let j = 0; j < puzzle[i].length; j++) {
         const node = domField.find(`[data-pos='${i},${j}']`);
+        node.removeAttr("style");
         if (puzzle[i][j] === 0) {
           node.addClass("empty");
           node.text("");
@@ -115,118 +122,120 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  function moveTile() {
-    // Gets the position of the current element
-    var pos = $(this).attr("data-pos");
-    var posRow = parseInt(pos.split(",")[0]);
-    var posCol = parseInt(pos.split(",")[1]);
+  const movePeace = async (domField) => {
+    await asyncForEach(npuzzle.path, async (direction) => {
+      await waitFor(250);
+      switch (direction) {
+        case "U":
+          moveUp(domField);
+          break;
+        case "D":
+          moveDown(domField);
+          break;
+        case "L":
+          moveLeft(domField);
+          break;
+        case "R":
+          moveRight(domField);
+          break;
+      }
+    });
+  };
 
-    // Move Up
-    if (posRow + 1 == emptytilePosRow && posCol == emptytilePosCol) {
-      $(this).animate({
-        "top": "+=" + cellDisplacement //moves up
-      });
+  const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
 
-      $("#empty").animate({
-        "top": "-=" + cellDisplacement //moves down
-      });
-
-      emptytilePosRow -= 1;
-      $(this).attr("data-pos", (posRow + 1) + "," + posCol);
+  async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
     }
-
-    // Move Down
-    if (posRow - 1 == emptytilePosRow && posCol == emptytilePosCol) {
-      $(this).animate({
-        "top": "-=" + cellDisplacement //moves down
-      });
-
-      $("#empty").animate({
-        "top": "+=" + cellDisplacement //moves up
-      });
-
-      emptytilePosRow += 1;
-      $(this).attr("data-pos", (posRow - 1) + "," + posCol);
-    }
-
-    // Move Left
-    if (posRow == emptytilePosRow && posCol + 1 == emptytilePosCol) {
-      $(this).animate({
-        "right": "-=" + cellDisplacement //moves right
-      });
-
-      $("#empty").animate({
-        "right": "+=" + cellDisplacement //moves left
-      });
-
-      emptytilePosCol -= 1;
-      $(this).attr("data-pos", posRow + "," + (posCol + 1));
-    }
-
-    // Move Right
-    if (posRow == emptytilePosRow && posCol - 1 == emptytilePosCol) {
-      $(this).animate({
-        "right": "+=" + cellDisplacement //moves left
-      });
-
-      $("#empty").animate({
-        "right": "-=" + cellDisplacement //moves right
-      });
-
-      emptytilePosCol += 1;
-      $(this).attr("data-pos", posRow + "," + (posCol - 1));
-    }
-
-    // Update empty position
-    $("#empty").attr("data-pos", emptytilePosRow + "," + emptytilePosCol);
   }
 
-  function start() {
-    // var init = new Node(0, [[6, 4, 7], [8, 5, 0], [3, 2, 1]], 1, 2, 0);
-    var init = new Node(0, [[7, 4, 0], [2, 6, 8], [1, 3, 5]], 0, 2, 0);
-    var goal = new Node(0, [[1, 2, 3], [4, 5, 6], [7, 8, 0]], 2, 2, 0);
+  const moveUp = (domField) => {
+    const peace = domField.find(`[data-pos='${npuzzle.emptyRow - 1},${npuzzle.emptyCol}']`);
+    const empty = $(".empty");
 
-    var astar = new AStar(init, goal, 0);
-    // To measure time taken by the algorithm
-    var startTime = new Date();
-    // Execute AStar
-    var result = astar.execute();
-    // To measure time taken by the algorithm
-    var endTime = new Date();
-    alert("Completed in: " + (endTime - startTime) + " milliseconds");
+    peace.animate({
+      "top": "+=" + MOVE_DISTANCE //moves up
+    }, 100);
 
-    var panel = document.getElementById("panel");
-    panel.innerHTML = "Solution: " + result.path + " Total steps: " + result.path.length + "<br>";
-    solution = result.path;
-  }
+    empty.animate({
+      "top": "-=" + MOVE_DISTANCE //moves down
+    }, 100);
 
-  var step = 0;
-  var solution = "";
+    npuzzle.emptyRow -= 1;
+    peace.attr("data-pos", (npuzzle.emptyRow + 1) + "," + npuzzle.emptyCol);
+    empty.attr("data-pos", (npuzzle.emptyRow) + "," + npuzzle.emptyCol);
+  };
 
-  function showSolution() {
+  const moveDown = (domField) => {
+    const peace = domField.find(`[data-pos='${npuzzle.emptyRow + 1},${npuzzle.emptyCol}']`);
+    const empty = $(".empty");
 
-    var move = "";
+    peace.animate({
+      "top": "-=" + MOVE_DISTANCE //moves up
+    }, 100);
 
-    switch (solution[step]) {
-      case "R":
-        move = (emptytilePosRow).toString() + "," + (emptytilePosCol + 1).toString();
-        break;
-      case "L":
-        move = (emptytilePosRow).toString() + "," + (emptytilePosCol - 1).toString();
-        break;
-      case "U":
-        move = (emptytilePosRow - 1).toString() + "," + (emptytilePosCol).toString();
-        break;
-      case "D":
-        move = (emptytilePosRow + 1).toString() + "," + (emptytilePosCol).toString();
-        break;
-    }
+    empty.animate({
+      "top": "+=" + MOVE_DISTANCE //moves down
+    }, 100);
 
-    $("div[data-pos='" + move + "']").click();
-    const panel = document.getElementById("panel");
-    panel.innerHTML += "Step: " + step + " -> " + solution[step] + " ,";
-    step++;
-  }
+    npuzzle.emptyRow += 1;
+    peace.attr("data-pos", (npuzzle.emptyRow - 1) + "," + npuzzle.emptyCol);
+    empty.attr("data-pos", (npuzzle.emptyRow) + "," + npuzzle.emptyCol);
+  };
+
+  const moveLeft = (domField) => {
+    const peace = domField.find(`[data-pos='${npuzzle.emptyRow},${npuzzle.emptyCol - 1}']`);
+    const empty = $(".empty");
+
+    peace.animate({
+      "right": "-=" + MOVE_DISTANCE //moves up
+    }, 100);
+
+    empty.animate({
+      "right": "+=" + MOVE_DISTANCE //moves down
+    }, 100);
+
+    npuzzle.emptyCol -= 1;
+    peace.attr("data-pos", npuzzle.emptyRow + "," + (npuzzle.emptyCol + 1));
+    empty.attr("data-pos", npuzzle.emptyRow + "," + npuzzle.emptyCol);
+  };
+
+  const moveRight = (domField) => {
+    const peace = domField.find(`[data-pos='${npuzzle.emptyRow},${npuzzle.emptyCol + 1}']`);
+    const empty = $(".empty");
+
+    peace.animate({
+      "right": "+=" + MOVE_DISTANCE //moves up
+    }, 100);
+
+    empty.animate({
+      "right": "-=" + MOVE_DISTANCE //moves down
+    }, 100);
+
+    npuzzle.emptyCol += 1;
+    peace.attr("data-pos", npuzzle.emptyRow + "," + (npuzzle.emptyCol - 1));
+    empty.attr("data-pos", npuzzle.emptyRow + "," + npuzzle.emptyCol);
+  };
+
+  let npuzzle = {
+    heuristic: "manh",
+    field: "3x3",
+    size: 3,
+    puzzle: [[]],
+    emptyRow: 2,
+    emptyCol: 2,
+    path: [],
+  };
+
+  $("#3x3").click(() => handleDrawField("3x3"));
+  $("#4x4").click(() => handleDrawField("4x4"));
+  $("#manhattanDistance").click(() => handleChangeHeuristic("manh"));
+  $("#linearConflicts").click(() => handleChangeHeuristic("lin"));
+  $("#misplacedTiles").click(() => handleChangeHeuristic("misp"));
+  $(".random-button").click(() => handleRandomizeField());
+  $(".solve-button").click(() => handleSolve());
+  $(".play-button").click(() => handlePlay());
 
   handleDrawField("3x3");
   handleRandomizeField();
